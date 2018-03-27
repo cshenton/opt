477ee241414ea6addbdb6cee24fb82530044eb11
cshenton <charlieshenton@me.com>
2018-03-27 12:56:59 +1100

    snes messages, notes

--- snes.go
+++ snes.go
@@ -24,30 +24,60 @@ func NewSNES() (s *SNES) {
 
 // Search returns a point and the seed used to draw it from the search distribution.
 func (s *SNES) Search() (point []float64, seed int64) {
-	// Sends a search request to search channel
-	// Gets response on response channel
-	// Will of course block in optimiser is handling a slow update
-	return
+	rc := make(chan searchResp)
+	s.searchChan <- searchReq{
+		respChan: rc,
+	}
+	r := <-rc
+	return r.point, r.seed
 }
 
 // Show updates the search distribution given a score and the seed that achieved it.
 func (s *SNES) Show(score float64, seed int64) {
-	// Sends show request to show channel
-	// returns
-	// we don't block on show, only on search
+	s.showChan <- showReq{
+		score: score,
+		seed:  seed,
+	}
 	return
 }
 
 // doSearch conducts Search assuming exclusive data structure access.
 func (s *SNES) doSearch() (point []float64, seed int64) {
+	// generate seed from rand
+	// use seed to draw randNorm
+	// scale and return point and seed
 	return
 }
 
 func (s *SNES) doShow(score float64, seed int64) {
+	// Add the results to the gen
+	// If the generation is complete
+	// compute score
+	// compute grads
+	// compute update
+	// if adaptive compute alternative update and test, update LR is necessary
+	// apply update
 	return
 }
 
+// !!!! STILL NEED TO BLOCK BETWEEN GENERATION SEND AND PROCESS
+// should be handled in run
+// Can we check each loop and say, "if the generation is allocated, listen on show and done"
+// "if the generation is yet to be allocated", listen on search, show and done
+// each pass atomatically updates the switch condition, so it's reliable
+
+// !!!! WHAT IF A WORKER THREAD DIES????
+// I mean, like a goroutine dies? More likely bad code.
+// No different to simple case, error handling is up to the user, if the generation isn't reported
+// on the optimiser will deadlock.
+// The only real way to fix this is either timeouts or a pulse, which I don't want to impose, the user
+// may choose to implement them. In such a case, I should possible provide an API for forcing search?
+// This really ties in to ownership and expiry of seeds.
+// We could preallocate blocks of seeds per generation, and only accept those.
+// Then we could provide an API to renew a seed, though TBH I would just prefer the user to return a default fitness.
+
 // run is the inner loop of the optimiser, and provides safe access to search data.
+// If a full generation of searches has been allocated
 func (s *SNES) run() {
 	for {
 		select {
